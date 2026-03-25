21b63dff104b2424df358d0fdc253ed248ce5385
Igor39-dev <19gorokhov90@gmail.com>
2026-03-25 14:28:45 +0200

    start project
    create proj,
    add apps (users, videos)
    connect to Postgres DB
    
    made migrations

---
+++ core/apps/videos/querysets.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.db.models import F, Q, Sum
+
+
+class VideoQuerySet(models.QuerySet):
+
+    def published(self, user=None):
+
+        if user and user.is_authenticated:
+            return self.filter(
+                Q(is_published=True) | Q(owner=user)
+            )
+
+        return self.filter(is_published=True)
+
+    def statistics(self):
+
+        return (
+            self
+            .filter(is_published=True)
+            .values("owner__username")
+            .annotate(
+                username=F("owner__username"),
+                likes_sum=Sum("total_likes")
+            )
+            .values("username", "likes_sum")
+            .order_by("-likes_sum")
+        )
---
+++ core/apps/videos/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+
+urlpatterns = []
+# urlpatterns = [
+
+#     path("v1/videos/", video_list),
+#     path("v1/videos/<int:pk>/", video_detail),
+#     path("v1/videos/<int:pk>/likes/", video_likes),
+
+#     path("v1/videos/ids/", video_ids),
+
+#     path("v1/videos/statistics-subquery/", video_statistics_subquery),
+#     path("v1/videos/statistics-group-by/", video_statistics_group),
+
+# ]
